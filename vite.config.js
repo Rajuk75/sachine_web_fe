5ef9627a8c1efbd3649862ca632ea72f3d244bcd
Raju Kumar <rajukumar957259@gmail.com>
2025-12-06 13:15:06 +0530

    fixes

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
     host: '0.0.0.0',
     port: process.env.PORT || 4173,
     allowedHosts: [
-      '',  // 👈 add your Render domain here
+      '',  // 👈 add your Render domain
     ],
   },
 })
